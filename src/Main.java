f83180e6e1dafdbb402d0e9d1275608b97e3628e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-11 21:32:17 +0300

    Collections.reverseOrder method

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Queue<Double> queue = new PriorityQueue<>();
+        Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
 
         queue.offer(3.0);
         queue.offer(2.5);
