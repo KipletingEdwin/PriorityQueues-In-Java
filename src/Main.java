7cb8ae32a798d00a1ed8f381bb1b406839f35734
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-11 21:35:38 +0300

    Change to priorityqueue of strings

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<String> queue = new PriorityQueue<>();
 
-        queue.offer(3.0);
-        queue.offer(2.5);
-        queue.offer(4.0);
-        queue.offer(1.5);
-        queue.offer(2.0);
+        queue.offer("B");
+        queue.offer("C");
+        queue.offer("A");
+        queue.offer("F");
+        queue.offer("D");
 
         while (!queue.isEmpty()){
             System.out.println(queue.poll());
